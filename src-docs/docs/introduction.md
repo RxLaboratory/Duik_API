82a5c8c05bed35a20b0a75d9ff7a07b431c801bd
Duduf <git@duduf.com>
2022-05-04 09:39:40 +0200

    Update DuAEF_Duik_api.jsxinc, Duik_api.jsxinc, and 181 more files...

---
+++ src-docs/docs/introduction.md
@@ -0,0 +1,3 @@
+![META](authors:Nicolas "Duduf" Dufresne;license:GNU-FDL;copyright:2022;updated:2022/05/04)
+
+# Introduction to the Duik API
---
+++ tools/build-doc.bat
@@ -0,0 +1,25 @@
+@echo off
+
+cd ..
+cd src-docs
+echo Building docs
+REM build doc
+mkdocs build
+echo Docs built
+cd ..
+REM build reference
+cd tools
+echo Building reference
+cmd /c jsdoc -c jsdoc_conf.json
+xcopy /Y ..\docs\reference\Duik.html ..\docs\reference\index.html
+echo Reference built
+cd ..
+cd docs
+REM add 404
+echo Adding 404 page
+echo " " > 404.md
+xcopy /Y ..\src-docs\assets\404.md 404.md
+REM add CNAME
+echo Creating CNAME
+echo duik.rxlab.io > "CNAME"
+echo Finished!
