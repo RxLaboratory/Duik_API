0e57260933db4a7ca700a7c500a191001713939c
Duduf <git@duduf.com>
2022-06-10 11:57:11 +0200

    new build using better-docs theme

--- tools/build-doc.bat
+++ tools/build-doc.bat
@@ -13,6 +13,8 @@ echo Building reference
 cmd /c jsdoc -c jsdoc_conf.json
 xcopy /Y ..\docs\reference\Duik.html ..\docs\reference\index.html
 echo Reference built
+REM add CSS
+xcopy /Y jsdoc.css ..\docs\reference
 cd ..
 cd docs
 REM add 404
